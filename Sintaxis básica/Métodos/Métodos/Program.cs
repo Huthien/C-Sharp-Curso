99f1439e08598fa49b4a639ef0a427aeff104c3a
Abby Corceiro <abbycorceiro@gmail.com>
2025-02-24 22:50:32 -0300

    Sintaxis básica

---
+++ Sintaxis básica/Métodos/Métodos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SE LLAMA AL MÉTODO QUE CORRESPONDE DEPENDIENDO SUS PARÁMETROS:
+
+            Console.WriteLine("Llamada del método suma con dos parámetros int: ");
+            Console.WriteLine($"El resultado de la suma es: {Suma(5, 7)}");
+            Console.WriteLine("\nLlamada del método suma con tres parámetros int: ");
+            Console.WriteLine($"El resultado de la suma es: {Suma(6, 8, 3)}");
+            Console.WriteLine("\nLlamada del método suma con un parámetro double y un parámetro int: ");
+            Console.WriteLine($"El resultado de la suma es: {Suma(2.5, 8)}");
+        }
+
+        //SOBRECARGA DE MÉTODOS:
+        static int Suma(int num1, int num2) => num1 + num2;
+        static int Suma(int num1, int num2, int num3) => num1 + num2 + num3;
+        static double Suma(double num1, int num2) => num1 + num2;
+    }
+}
---
+++ Sintaxis básica/Operaciones/Operaciones/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Operaciones
+{
+    class Program
+    {
+
+        static int num1;
+        static int num2;
+        static void Main(string[] args)
+        {
+            //SUMA DE DOS NÚMERO ENTEROS CON LLAMADO A MÉTODO
+            Console.WriteLine("SUMA DE DOS NÚMEROS:");
+            Console.WriteLine("Inserte un número: ");
+            num1 = userInput();
+            Console.WriteLine("Inserte otro número: ");
+            num2 = userInput();
+            Console.WriteLine($"La suma de los números es: {Suma(num1, num2)}");
+
+            //RESTA DE DOS NÚMEROS ENTEROS CON LLAMADA A MÉTODO
+            Console.WriteLine("\nRESTA DE DOS NÚMEROS:");
+            Console.WriteLine("Inserte un número: ");
+            num1 = userInput();
+            Console.WriteLine("Inserte otro número: ");
+            num2 = userInput();
+            Console.WriteLine($"La resta de los números es: {Resta(num1, num2)}");
+
+            //MULTIPLICACIÓN DE DOS NÚMEROS ENTEROS CON LLAMADA A MÉTODO
+            Console.WriteLine("\nMULTIPLICACIÓN DE DOS NÚMEROS:");
+            Console.WriteLine("Inserte un número: ");
+            num1 = userInput();
+            Console.WriteLine("Inserte otro número: ");
+            num2 = userInput();
+            Console.WriteLine($"La multiplicación de los números es: {Multiplicación(num1, num2)}");
+
+            //DIVISIÓN DE DOS NÚMEROS ENTEROS CON LLAMADA A MÉTODO
+            Console.WriteLine("\nDIVISIÓN DE DOS NÚMEROS:");
+            Console.WriteLine("Inserte un número: ");
+            num1 = userInput();
+            Console.WriteLine("Inserte otro número: ");
+            num2 = userInput();
+            Console.WriteLine($"La división de los números es: {División(num1, num2)}");
+        }
+
+        static int Suma(int num1, int num2) => num1 + num2;
+        static int Resta(int num1, int num2) => num1 - num2;
+        static int Multiplicación(int num1, int num2) => num1 * num2;
+        static int División(int num1, int num2) => num1 / num2;
+
+        static int userInput()
+        {
+            int input = int.Parse(Console.ReadLine());
+            return input;
+        }
+    }
+}
