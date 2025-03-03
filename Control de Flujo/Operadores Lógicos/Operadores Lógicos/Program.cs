73f122c920a8267d914622206abca1c3aab7f31f
Abby Corceiro <abbycorceiro@gmail.com>
2025-03-03 16:17:45 -0300

    Módulo 2

--- Control de Flujo/Operadores Lógicos/Operadores Lógicos/Program.cs
+++ Control de Flujo/Operadores Lógicos/Operadores Lógicos/Program.cs
@@ -7,6 +7,7 @@ namespace Operadores_Lógicos
         static double primerTri;
         static double segundoTri;
         static double tercerTri;
+        static int faltas;
         static void Main(string[] args)
         {
             //Se piden los valores de las notas 
@@ -17,10 +18,12 @@ namespace Operadores_Lógicos
             segundoTri = double.Parse(Console.ReadLine());
             Console.WriteLine("Nota registrada del tercer trimestre: ");
             tercerTri = double.Parse(Console.ReadLine());
+            Console.WriteLine("Posees faltas? De ser asi introduce la cantidad de faltas: ");
+            faltas = int.Parse(Console.ReadLine());
 
             if (primerTri < 1 || primerTri > 10 || segundoTri < 1 || segundoTri > 10 || tercerTri < 1 || tercerTri > 10)
             {
-                Console.WriteLine("ERROR - Has introducido un número inválido");
+                Console.WriteLine("Has introducido una nota inválida --- Las notas no pueden ser menor a 1");
             }
             else CheckNotes();
         }
@@ -29,10 +32,14 @@ namespace Operadores_Lógicos
         static void CheckNotes() 
         {
             double resultado = (primerTri + segundoTri + tercerTri) / 3;
-            if (resultado >= 7 ) 
+            if (resultado >= 7 && faltas < 10)
             {
                 Console.WriteLine("Felicitaciones! Has aprobado la materia!");
                 Console.WriteLine($"La nota de promedio es de: {resultado}");
+            }
+            else if (resultado >= 7 && faltas >= 10)
+            {
+                Console.WriteLine("Debido a las faltas deberás cursar 3 días de presencial y rendir un exámen educativo que se deberá aprobar con un nota mayor a 7 para validar el promedio de alumno regular");
             }
             else Console.WriteLine("Lo siento, deberás recursar la materia...");
         }
---
+++ Control de Flujo/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+
+        static int numMes;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elije el número de mes:");
+            numMes = int.Parse(Console.ReadLine());
+            CheckMonth();
+        }
+
+        static void CheckMonth() 
+        {
+            switch (numMes)
+            {
+                case 1:
+                    Console.WriteLine("Mes de Enero: 31 días");
+                    break;
+                case 2:
+                    Console.WriteLine("Mes de Febrero: 28 días");
+                    break;
+                case 3:
+                    Console.WriteLine("Mes de Marzo: 31 días");
+                    break;
+                case 4:
+                    Console.WriteLine("Mes de Abril: 30 días");
+                    break;
+                case 5:
+                    Console.WriteLine("Mes de Mayo: 31 días");
+                    break;
+                case 6:
+                    Console.WriteLine("Mes de Junio: 30 días");
+                    break;
+                case 7:
+                    Console.WriteLine("Mes de Julio: 31 días");
+                    break;
+                case 8:
+                    Console.WriteLine("Mes de Agosto: 31 días");
+                    break;
+                case 9:
+                    Console.WriteLine("Mes de Septiembre: 30 días");
+                    break;
+                case 10:
+                    Console.WriteLine("Mes de Octubre: 31 días");
+                    break;
+                case 11:
+                    Console.WriteLine("Mes de Noviembre: 30 días");
+                    break;
+                case 12:
+                    Console.WriteLine("Mes de Diciembre: 31 días");
+                    break;
+                default:
+                    break;
+            }
+               
+        }
+    }
+}
