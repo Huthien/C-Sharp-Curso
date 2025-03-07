43f6aef41af63c4ce18231dbfa150ab68c8ce6dd
Abby Corceiro <abbycorceiro@gmail.com>
2025-03-07 16:43:03 -0300

    POO

---
+++ POO-Programación Orientada a Objetos/Encapsulación/Encapsulación/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Encapsulación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorEuroDolar conversor = new ConversorEuroDolar();
+            Console.WriteLine("Conversor de Euro a Dolar");
+            Console.WriteLine("Ingresa una cantidad: ");
+            double userInput = double.Parse(Console.ReadLine());
+            Console.WriteLine($"La cantidad ingresada equivale a: {conversor.Convert(userInput)}");
+
+            //En el caso de que el valor del euro cambie, podemos modificarlo a través de un método
+            //public desde la clase EuroDolar, pero manteniendo el encapsulamiento de la variable euro
+            //en private
+        }
+    }
+ 
+    class ConversorEuroDolar
+    {
+        //Se encapsula el dato para que no sea accesible desde fuera de la clase
+        //No es necesario colocar el private porque sin él la variable ya está encapsulada, 
+        //pero es recomendable utilizarlo en programas complejos para que a simple vista se sepa
+        //que la variable está encapsulada.
+        private double euro = 1.08;
+
+        public double Convert(double cantidad) 
+        { 
+            return cantidad * euro;
+        }
+
+        public void NuevoValor(double valor) 
+        {
+            if (valor < 0) euro = 1.08;
+            else euro = valor;
+
+            //Se checkea que el usuario no cambie el valor del euro con un valor negativo,
+            //porque de ser asi se quedará el valor por defecto
+            //ÉSste método se puede llamar desde la clase Program, método Main para modificar el valor
+            //del euro
+        }
+    }
+}
---
+++ POO-Programación Orientada a Objetos/POO/POO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se declara e inicia una variable objeto de tipo Circulo con la palabra reservada new:
+
+            Circulo circulo = new Circulo(); 
+            Console.WriteLine("Área de un círculo!");
+            Console.WriteLine("Introduce el radio de tu círculo: ");
+            double userInput = double.Parse(Console.ReadLine());
+
+            //Se llama al método AreaCircular de la clase Circulo desde la clase Programa con cw:
+            Console.WriteLine(circulo.AreaCircular(userInput));
+        }
+    }
+
+    class Circulo 
+    {
+        
+        private const double pi = 3.1416; 
+
+        //El modificador de acceso public nos permite acceder al método desde fuera de la clase:
+        public double AreaCircular(double radio)
+        {
+            return Math.Pow(radio, 2) * pi; 
+            //En el curso se utilizo pi * radio * radio, pero yo decidí usar el método Math.Pow
+            //para calcular el radio al cuadrado y luego multiplicarlo por la variable pi
+        }   
+    }
+}
