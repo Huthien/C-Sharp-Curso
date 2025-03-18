0aa3a4ec82a222eae7131f0c0bbee432317adee3
Abby Corceiro <abbycorceiro@gmail.com>
2025-03-18 15:03:25 -0300

    Herencia

--- POO-Herencia/EjercicioHerencia/EjercicioHerencia/Program.cs
+++ POO-Herencia/EjercicioHerencia/EjercicioHerencia/Program.cs
@@ -13,6 +13,8 @@ namespace EjercicioHerencia
 
             Console.WriteLine("Propiedades de un avión:");
             avion.ArrancarMotor();
+            avion.Despegar();
+            avion.Aterrizar();
             avion.PararMotor();
             avion.Conducir();
 
@@ -24,7 +26,7 @@ namespace EjercicioHerencia
             Console.WriteLine("\nPropiedades de una moto:");
             moto.ArrancarMotor();
             moto.PararMotor();
-            moto.Conducir();
+            moto.Conducir(); //Usa el método de la clase padre porque no tiene un método conducir override propio.
         }
     }
 
@@ -40,12 +42,16 @@ namespace EjercicioHerencia
     {
         public Avion() : base() { }
         public override void Conducir() => Console.WriteLine("-Se pilotea de manera manual o automática");
+
+        //Métodos propios de un avión:
+        public void Despegar() => Console.WriteLine("-Puede despegar");
+        public void Aterrizar() => Console.WriteLine("-Puede Aterrizar");
     }
 
     class Auto :Vehiculo
     { 
         public Auto() : base() { }
-        public override void Conducir() => Console.WriteLine("-Se puede conducir de manera manual y automática");
+        public override void Conducir() => Console.WriteLine("-Se conduce de manera manual o automática");
     }
 
     class Moto : Vehiculo 
