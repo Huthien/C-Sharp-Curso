55f44c670ae1bdec3ee10052074a7d39750fdbb7
Abby Corceiro <abbycorceiro@gmail.com>
2025-03-20 11:38:48 -0300

    Clases Abstractas

---
+++ ClasesAbstractas/ClasesAbstractas/ClasesAbstractas/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ClasesAbstractas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cracterísticas de un cangrejo:
+            Cangrejo cangrejo = new Cangrejo("Cangrejo");
+            cangrejo.getNombre();
+            Console.WriteLine($"Número de patas: {cangrejo.NumPatas()}");
+            cangrejo.Desplazarse();
+
+            //Características de una Abeja:
+            Abeja abeja = new Abeja("Abeja");
+            abeja.getNombre();
+            Console.WriteLine($"Número de patas: {abeja.NumPatas()}");
+            abeja.HacerMiel();
+            Console.WriteLine($"-Es capaz de volar? = {abeja.Volar()}");
+
+            //Características de una Hormiga:
+            Hormiga hormiga = new Hormiga("Hormiga");
+            hormiga.getNombre();
+            Console.WriteLine($"Número de patas: {hormiga.NumPatas()}");
+            hormiga.Transportar();
+            Console.WriteLine($"-Es capaz de volar? = {hormiga.Volar()}");
+
+            //Características de un Mosquito:
+            Mosquito mosquito = new Mosquito("Mosquito");
+            mosquito.getNombre();
+            Console.WriteLine($"Número de patas: {mosquito.NumPatas()}");
+            mosquito.ChuparSangre();
+            Console.WriteLine($"-Es capaz de volar? = {mosquito.Volar()}");
+        }
+    }
+
+    //Se declara una clase abstracta por encima de las demás:
+    abstract class Artrópodos 
+    {
+        //Método abstracto que heredarán las subclases que lo requieren:
+        abstract public int NumPatas();
+        abstract public void getNombre();
+    }
+    class Insectos : Artrópodos //Clase padre Insectos
+    {
+        String nombre;
+        int patas;
+        bool volar;
+        public Insectos(String nombre) => this.nombre = nombre;
+
+        //Métodos override heredados de la clase abstracta
+        public override void getNombre() => Console.WriteLine($"\nNombre del Insecto: {nombre}");
+        public override int NumPatas() => 6;
+
+        //Método propio de la clase insecto, heredado por sus subclases
+        public virtual bool Volar() => volar;
+    }
+
+    class Abeja : Insectos //Subclase de Insectos
+    { 
+        public Abeja(String nombre):base(nombre) { }
+
+        //Método propio de la clase
+        public void HacerMiel() => Console.WriteLine("-Son capaces de crear miel en sus panales");
+
+        public override bool Volar() => true; //Método heredado de la clase Insectos
+    }
+
+    class Hormiga :Insectos //Subclase de Insectos
+    {
+        public Hormiga(String nombre) : base(nombre) { }
+
+        //Método propio de la clase
+        public void Transportar() => Console.WriteLine("-Pueden transportar cargas de hasta 50 veces su peso con sus mandíbulas");
+
+        public override bool Volar() => false; //Método heredado de la clase Insectos
+    }
+
+    //La palabra reservada sealed sella la clase mosquito
+    sealed class Mosquito : Insectos //Subclase de Insectos
+    {
+        public Mosquito(String nombre) : base(nombre) { }
+
+        //Método propio de la clase
+        public void ChuparSangre() => Console.WriteLine("-Se alimentan de absorber sangre de otros animales");
+
+        public override bool Volar() => true; //Método heredado de la clase Insectos
+    }
+
+    class Cangrejo : Artrópodos //No es un insecto por lo que hereda de la clase abstracta Artrópodos
+    {
+        int patas;
+        string nombre;
+
+        public Cangrejo(String nombre) => this.nombre = nombre;
+
+        //Método propio de la clase
+        public void Desplazarse() => Console.WriteLine("-Puede desplazarse de lado");
+
+        //Métodos heredados de la clase abstracta
+        public override int NumPatas() => 10;
+        public override void getNombre() => Console.WriteLine($"\nNombre del Artrópodo: {nombre}");
+    }
+
+    //La palabra reservada sealed sirve para sellar una clase para que otras no puedan heredas de ella
+    //También sirve para sellar métodos para que no puedan ser sobreescritos por otras subclases con override
+}
---
+++ ClasesAbstractas/Properties/Properties/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado = new Empleado("Hernán");
+
+            //Uso del setter de la propertie
+            empleado.SALARIO = 1500;
+            //Uso del getter de la propertie
+            Console.WriteLine($"El salario inicial del empleado es: {empleado.SALARIO}");
+
+            empleado.SALARIO += 500; //Se incrementa el valor y se imprime en pantalla
+            Console.WriteLine($"El salario incrementado del empleado es: {empleado.SALARIO}");
+
+            //Si el salario es negativo (menor a 0), entonces a través de la propertie se detecta
+            empleado.SALARIO -= 2300;
+            Console.WriteLine($"El salario en NEGATIVO del empleado es: {empleado.SALARIO}");
+        }
+    }
+
+    class Empleado 
+    {
+        String _nombre;
+        double _salario;
+        public Empleado(String nombre) => this._nombre = nombre;
+
+        //Se crea el método para evaluar si el salario es menor a 0
+        private double evaluaSalario(double salario) 
+        {
+            if (salario < 0) return 0;
+            else return salario;
+        }
+
+        //Creación de la propertie:
+        public double SALARIO 
+        { 
+            //Se crea un getter y un setter dentro de la propertie
+            get => this._salario; 
+            set => this._salario = evaluaSalario(value); 
+        }
+    }
+
+    //Las properties son útiles en ciertos escenarios pero tienen una sintaxis mas complicada que simplemente
+    //encapsular los campos de clase.
+
+    //Colocar un guion bajo en la variable de campo permite distinguirla de otras con el mismo nombre y no figura 
+    //en el menú intellisense 
+
+    //Se pueden crear properties de SOLO ESCRITURA (Como las contraseñas) prescindiendo del get en dicha propertie.
+    //Lo mismo pasa con properties de SOLO LECTURA, se prescinde del set.
+}
