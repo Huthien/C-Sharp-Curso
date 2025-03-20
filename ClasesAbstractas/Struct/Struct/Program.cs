f3f07a05d7c1fac4d12a1b2d3ef571ea7ad84278
Abby Corceiro <abbycorceiro@gmail.com>
2025-03-20 15:31:42 -0300

    Clases Abstractas

---
+++ ClasesAbstractas/Struct/Struct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se crea un objeto de tipo empleado son su salario y comision
+            Empleado emp1 = new Empleado(2500, 300);
+            Console.WriteLine(emp1);
+
+            //Se incrementa el salario y la comision, pero los cambios no se ven reflejados porque Empleado es struct
+            emp1.cambiaSalario(emp1, 100);
+            Console.WriteLine(emp1);
+            
+        }
+    }
+
+    struct Empleado //Se almacena en la memoria stack y los cambios solo afectan a la copia en vez de a la estructura
+    {
+        private double salario;
+        private double comision;
+        public Empleado(int salario, int comision)
+        {
+            this.salario = salario;
+            this.comision = comision;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Salario del empleado: {0} \nComisión: {1}", this.salario, this.comision);
+        }
+
+        public void cambiaSalario(Empleado em, double incremento)
+        {
+            em.salario += incremento;
+            em.comision += incremento;
+        }
+    }
+}
---
+++ ClasesAbstractas/TiposEnumerados/TiposEnumerados/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TiposEnumerados
+{
+    //Se declara el enum. Suelen declararse en el namespace:
+    enum Bonus { Bajo = 500, Normal = 1000, Bueno = 1500, Extra = 2500 }; //Son constantes sin tipo de dato, no son string!
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se crea un objeto del tipo del enum creado y se le dice que es igual a una de las constantes del enum:
+            Bonus puntos = Bonus.Normal;
+            //Para guardar un valor nulo en un objeto de tipo enum se coloca el ? al final del nombre del enum
+            //Bonus? puntaje = null; 
+            Console.WriteLine($"Bonus de puntos: {puntos}"); //Imprime el tipo de bonus que se recibe
+
+            //Para obtener el valor almacenado en la constante hay que hacer un casting:
+            double puntaje = (double)puntos;
+            Console.WriteLine($"\nBonus de puntos: {puntaje}");
+
+            //Los valores se pueden incremtentar:
+            double nuevoPuntaje = 1500 + puntaje;
+            Console.WriteLine($"\nEl nuevo puntaje es: {nuevoPuntaje}");
+
+            //Se crea un objeto Jugador
+            Jugador jugador = new Jugador(Bonus.Extra, 1500);
+            Console.WriteLine($"\nEl puntaje del jugador es: {jugador.getPuntaje()}");
+        }
+    }
+
+    class Jugador 
+    {
+        private double puntaje, bonus;
+
+        public Jugador(Bonus bonus, double puntaje) //Se pide un objeto de tipo enum como parámetro
+        {
+            this.bonus = (double)bonus; //Se hace un casting 
+            this.puntaje = puntaje;
+        }
+
+        public double getPuntaje() => puntaje + bonus;
+    }
+}
