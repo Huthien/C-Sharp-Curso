73cf54ea9fdbfc978c6d25cd036b94c77507ca48
Abby Corceiro <abbycorceiro@gmail.com>
2025-03-21 10:23:18 -0300

    Clases Abstractas

---
+++ ClasesAbstractas/Destructores/Destructores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Destructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos mi_archivo = new ManejoArchivos();
+            mi_archivo.Mensaje();
+        }
+    }
+
+    class ManejoArchivos
+    {
+        StreamReader archivo = null;
+        int contador = 0;
+        string linea;
+
+        public ManejoArchivos() 
+        {
+            archivo = new StreamReader(@"PATH"); //Se reemplaza PATH con el directorio del archivo txt a leer
+
+            while ((linea = archivo.ReadLine()) != null) 
+            {
+                Console.WriteLine(linea);
+                contador++;
+            }
+        }
+
+        public void Mensaje() => Console.WriteLine("Hay {0} lineas", contador);
+
+        //Creación del destructor (Se debe llamar igual a la clase, como los constructores)
+        ~ManejoArchivos() => archivo.Close();
+    }
+}
---
+++ ClasesAbstractas/Destructores/Destructores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Destructores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Destructores")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea4e3d9e-0c0f-4178-8f1f-ce79d7eea6e4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
