89b54cb18818c8a6090e7ec13c166802798db260
Abby Corceiro <abbycorceiro@gmail.com>
2025-04-14 16:36:19 -0300

    Threads

---
+++ Threads/CancelacionTask/CancelacionTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelacionTask
+{
+    class Program
+    {
+        static int acumulador = 0;
+        static void Main(string[] args)
+        {
+            //El cancellation token source señala qué cancellation token es el que cancelará la task
+            CancellationTokenSource miToken = new CancellationTokenSource();
+            CancellationToken cancelaToken = miToken.Token;//Se crea el token que cancela
+
+            Task t1 = Task.Run(() => EjecutarTarea(cancelaToken));
+
+            for (int i = 0; i < 100; i++) 
+            { 
+                acumulador += 30;
+                Thread.Sleep(1000);
+                //Se chequea si la variable acumulador es mayor a 100, y si es así se cancela la tarea
+                if (acumulador > 100) 
+                { 
+                    miToken.Cancel();
+                    break;//Se sale del bucle
+                }
+            }
+           
+            Thread.Sleep(1000);
+            Console.WriteLine($"VALOR DE ACUMULADOR: {acumulador}");
+            Console.ReadLine();
+        }
+
+        //Se le indica al método a través de un parámetro que puede ser cancelado bajo ciertas circunstancias:
+        static void EjecutarTarea(CancellationToken tk) 
+        {
+            for (int i = 0; i < 100; i++) 
+            {
+                acumulador++;
+                var t = Thread.CurrentThread.ManagedThreadId;
+                Thread.Sleep(500);
+                Console.WriteLine($"El hilo N°: {t} está ejecutando la tarea");
+                Console.WriteLine(acumulador);
+                if (tk.IsCancellationRequested) 
+                {
+                    acumulador = 0;
+                    return; 
+                }
+            }
+        }
+    }
+}
---
+++ Threads/ClaseParallel/ClaseParallel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClaseParallel
+{
+    internal class Program
+    {
+
+        private static int acumulador = 0;
+        static void Main(string[] args)
+        {
+            //Se usa la clase Parallel y su método For para simplificar el código y reemplazar el bucle for tradicional
+            //Se pasa como primer parámetro el valor de inicio, le sigue el valor al que llegaría, y luego el nombre del
+            //método a ejecutar:
+            Parallel.For(0,100,RealizarTarea);
+        }
+
+        static void RealizarTarea(int dato) 
+        {
+            Console.WriteLine($"Acumulador vale: {acumulador}. Tarea realizada por el hilo N°: {Thread.CurrentThread.ManagedThreadId}");
+            if ((acumulador % 2) == 0)
+            {
+                acumulador += dato;
+                Thread.Sleep(100);
+            }
+            else 
+            { 
+                acumulador -= dato;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
